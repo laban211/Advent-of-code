bee9cba756b25ed8c08a7f3b65361acd5f899ed7
Daniel Tabacsko <Daniel.Tabacsko@semcon.com>
2019-12-04 11:53:42 +0100

    some cleanup

--- 04/main.py
+++ 04/main.py
@@ -7,7 +7,7 @@ def main():
 def num_of_corr_passwords(start, stop):
     num = 0
     for i in range(start, stop):
-        if is_correct_password(i) and new_rules(i):
+        if new_rules(i):
             num += 1
 
     return num
@@ -34,16 +34,13 @@ def new_rules(password):
 
     password_str = str(password)
     has_duplet = False
-    has_triplet = False
-    my_dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+    occ_counter = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
 
     for num in password_str:
-        my_dict[int(num)] = my_dict[int(num)] + 1
+        occ_counter[int(num)] = occ_counter[int(num)] + 1
 
     for i in range(1, 10):
-        if my_dict.get(i) == 3:
-            has_triplet = True
-        elif my_dict.get(i) == 2:
+        if occ_counter.get(i) == 2:
             has_duplet = True
 
     return True if has_duplet else False
